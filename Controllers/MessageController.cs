ed0c85159d2b9312845ad29d0fd06b6669bcde66
chi <gochichi2016@gmail.com>
2024-07-14 14:46:55 +0800

    匯入商品前的存檔

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace escale.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: MessageController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
