c9671c0329ae335eb3bb52288c0c07306c1f235d
go chichi <gochichi2016@gmail.com>
2024-07-16 11:23:01 +0800

    15.3做但因老師命名更改自己的about相關變contact頁面就出不來了

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace escale.Controllers
+{
+    public class AboutController : Controller
+    {
+        [HttpGet]
+public IActionResult Contact()
+{
+    var model = new vmContact();
+    return View(model);
+}
+
+[HttpPost]
+public IActionResult Contact(vmContact model)
+{
+    if (!ModelState.IsValid) return View(model);
+
+    //寄信給系統管理者及訊息提交者
+    using var sendEmail = new SendMailService();
+
+    var mailObject = new MailObject();
+    mailObject.MailTime = DateTime.Now;
+    mailObject.UserNo = "";
+    mailObject.UserName = "";
+    mailObject.ToName = model.ContactorName;
+    mailObject.ToEmail = model.ContactorEmail;
+    mailObject.MailSubject = model.ContactorSubject;
+    mailObject.MailContent = model.ContactorMessage;
+
+    SessionService.MessageText = sendEmail.ContactUs(mailObject);
+
+    //顯示完成訊息
+    SessionService.StringValue1 = "ShopIndex";
+    if (string.IsNullOrEmpty(SessionService.MessageText))
+        SessionService.MessageText = "您的留言訊息已提交，我們會使用電子信箱方式盡速回覆您的問題。";
+
+    return RedirectToAction("Index", "Message", new { area = "" });
+}
+    }
+}
