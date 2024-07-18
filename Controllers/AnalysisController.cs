e04af2dcb198ef81ecbfc5b2c4ec848a104e2896
go chichi <gochichi2016@gmail.com>
2024-07-18 14:09:49 +0800

    分析好了但電子秤資料串連未

---
+++ Controllers/AnalysisController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace escale.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        // GET: AnalysisController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
